b3ef6d2a53225fd97b1d776aace34df07094e328
Matt Wells <mattwells@users.noreply.github.com>
2024-03-18 21:11:26 +0000

    Add ability to delete tasks

--- app/Filament/Resources/TaskResource.php
+++ app/Filament/Resources/TaskResource.php
@@ -78,6 +78,9 @@ class TaskResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
